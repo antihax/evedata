b5d5e649def1df03d0a49b3e5a69cca4e4145390
antihax@gmail.com <antihax@gmail.com>
2016-09-21 19:56:57 -0500

    Bug: stop local site opening in zkill window.

--- static/js/site.js
+++ static/js/site.js
@@ -5,10 +5,10 @@
     function aggressorFormatter(value, row) {
     	var warURL = 'https://zkillboard.com/war/' + row.warID +  '/'
     	var entityURL = '/'+ row.aggressorType + '?id=' + row.aggressorID 
-        return '<a href="' + warURL + '" target="zkill"><i class="glyphicon glyphicon-star"></i></a> <a href="' + entityURL + '" target="zkill">' + value + '</a>';
+        return '<a href="' + warURL + '" target="zkill"><i class="glyphicon glyphicon-star"></i></a> <a href="' + entityURL + '">' + value + '</a>';
     }
     function defenderFormatter(value, row) {
     	var warURL = 'https://zkillboard.com/war/' + row.warID +  '/'
     	var entityURL = '/'+ row.defenderType + '?id=' + row.defenderID 
-        return '<a href="' + warURL + '" target="zkill"><i class="glyphicon glyphicon-star"></i></a> <a href="' + entityURL + '" target="zkill">' + value + '</a>';
+        return '<a href="' + warURL + '" target="zkill"><i class="glyphicon glyphicon-star"></i></a> <a href="' + entityURL + '">' + value + '</a>';
     }
