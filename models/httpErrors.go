51082112b9a18b2062bf6868aede478f215bfea2
antihax@gmail.com <antihax@gmail.com>
2017-01-06 20:25:18 -0600

    move to new schema

--- models/httpErrors.go
+++ models/httpErrors.go
@@ -9,7 +9,7 @@ func AddHTTPError(req *http.Request, res *http.Response) {
 	reqText, _ := httputil.DumpRequest(req, true)
 	resText, _ := httputil.DumpResponse(res, true)
 
-	database.Exec(`INSERT  INTO httpErrors 
+	database.Exec(`INSERT INTO evedata.httpErrors 
                     (url, status, request, response, time) 
                     VALUES(?,?,?,?,NOW());`,
 		req.URL.String(), res.StatusCode, reqText, resText)
