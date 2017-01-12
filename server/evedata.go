54043857a35ac010e3daa8845b4ecfc1ddd921a9
antihax@gmail.com <antihax@gmail.com>
2017-01-11 21:37:59 -0600

    Apparently using too many scioes

--- server/evedata.go
+++ server/evedata.go
@@ -85,11 +85,7 @@ func GoServer() {
 	}
 
 	// Setup the SSO authenticator, this is the main login.
-	ssoScopes := []string{
-		eveapi.ScopeCharacterLocationRead,
-		eveapi.ScopeCharacterNavigationWrite,
-		eveapi.ScopeRemoteClientUI,
-	}
+	ssoScopes := []string{}
 
 	ctx.SSOAuthenticator = eveapi.NewSSOAuthenticator(
 		ctx.HTTPClient,
@@ -105,21 +101,13 @@ func GoServer() {
 		eveapi.ScopeCharacterResearchRead,
 		eveapi.ScopeCharacterWalletRead,
 		"esi-assets.read_assets.v1",
-		"esi-bookmarks.read_character_bookmarks.v1",
-		"esi-calendar.read_calendar_events.v1",
-		"esi-calendar.respond_calendar_events.v1",
 		"esi-characters.read_contacts.v1",
 		"esi-characters.write_contacts.v1",
-		"esi-clones.read_clones.v1",
 		"esi-corporations.read_corporation_membership.v1",
-		"esi-fleets.read_fleet.v1",
-		"esi-fleets.write_fleet.v1",
-		"esi-killmails.read_killmails.v1",
 		"esi-location.read_location.v1",
 		"esi-location.read_ship_type.v1",
 		"esi-planets.manage_planets.v1",
 		"esi-search.search_structures.v1",
-		"esi-skills.read_skillqueue.v1",
 		"esi-skills.read_skills.v1",
 		"esi-ui.open_window.v1",
 		"esi-ui.write_waypoint.v1",
