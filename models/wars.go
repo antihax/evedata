32a9fcaececcc58540ad1c5df741933c3ae222e8
antihax@gmail.com <antihax@gmail.com>
2017-03-11 21:05:46 -0600

    correct issue with CCP list limitation

--- models/wars.go
+++ models/wars.go
@@ -265,19 +265,25 @@ func GetFactionWarEntitiesForID(factionID int32) ([]FactionWarEntities, error) {
 		return nil, errors.New("Unknown FactionID")
 	}
 
-	wars := FactionsAtWar[factionID]
-	w := []FactionWarEntities{}
-	if err := database.Select(&w, `
-	SELECT 
-		DISTINCT IF(C.allianceID > 0, C.allianceID, corporationID) AS id,
-		IF(C.allianceID > 0, A.name, C.name) AS name,
-		IF(C.allianceID > 0, "alliance", "corporation") AS type 
-		FROM evedata.corporations C 
-		LEFT OUTER JOIN evedata.alliances A ON C.allianceID = A.allianceID
-		INNER JOIN evedata.entityKillStats K ON K.id = IF(C.allianceID > 0, C.allianceID, C.corporationID)
-		WHERE factionID IN (?, ?) AND (C.memberCount > 8 OR K.kills > 0 OR K.losses > 0);
-		`, wars[0], wars[1]); err != nil {
-		return nil, err
+	// Due to CCP limitation, make sure count is under 1024, cut stuff off until it is.
+	for i := 1; i < 20; i++ {
+		wars := FactionsAtWar[factionID]
+		w := []FactionWarEntities{}
+		if err := database.Select(&w, `
+			SELECT 
+				DISTINCT IF(C.allianceID > 0, C.allianceID, corporationID) AS id,
+				IF(C.allianceID > 0, A.name, C.name) AS name,
+				IF(C.allianceID > 0, "alliance", "corporation") AS type 
+				FROM evedata.corporations C 
+				LEFT OUTER JOIN evedata.alliances A ON C.allianceID = A.allianceID
+				INNER JOIN evedata.entityKillStats K ON K.id = IF(C.allianceID > 0, C.allianceID, C.corporationID)
+				WHERE factionID IN (?, ?) AND (C.memberCount > ? OR K.kills > 0 OR K.losses > 0);
+			`, i, wars[0], wars[1]); err != nil {
+			return nil, err
+		}
+		if len(w) < 1024 {
+			return w, nil
+		}
 	}
-	return w, nil
+	return nil, nil
 }
