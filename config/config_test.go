d3eead93fbe64de28d58249a4893234c339a2f72
Danni <antihax@gmail.com>
2017-04-09 13:04:56 -0500

    Test the example for travis-ci

--- config/config_test.go
+++ config/config_test.go
@@ -3,7 +3,7 @@ package config
 import "testing"
 
 func TestAppContext(t *testing.T) {
-	_, err := ReadConfig("config.conf")
+	_, err := ReadConfig("config-example.conf")
 	if err != nil {
 		t.Error(err)
 		return
