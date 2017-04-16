7858dcd6c4af044a3505fbf2bdb1e447e8cff9ff
antihax <antihax@gmail.com>
2017-04-16 18:45:10 -0500

    Update association test

--- models/associations_test.go
+++ models/associations_test.go
@@ -40,17 +40,36 @@ func TestGetAllianceKnownAssociates(t *testing.T) {
 }
 
 func TestBuildRelationships(t *testing.T) {
-	err := UpdateCorporationHistory(1001, 147035273, 100000222, time.Now())
+
+	err := UpdateCharacter(1001, "dude", 1, 1, 147035273, 0, 1, "male", -10, time.Now())
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	err = UpdateCorporationHistory(1002, 147035273, 100000222, time.Now())
+
+	err = UpdateCharacter(1002, "dude 2", 1, 1, 147035273, 0, 2, "Female", -10, time.Now())
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	err = UpdateCorporationHistory(1003, 147035273, 100000222, time.Now())
+
+	err = UpdateCharacter(1003, "dude 3", 1, 1, 147035273, 0, 2, "Female", -10, time.Now())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = UpdateCorporationHistory(1001, 147035273, 100000222, time.Now())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	err = UpdateCorporationHistory(1002, 147035273, 100000223, time.Now())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	err = UpdateCorporationHistory(1003, 147035273, 100000224, time.Now())
 	if err != nil {
 		log.Fatal(err)
 		return
