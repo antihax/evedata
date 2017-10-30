4cd85c88adfcc6eb4ae6ab8d450593f5a85ac36a
antihax <antihax@gmail.com>
2017-10-29 17:39:17 -0700

    add alliance nail

--- services/nail/entities.go
+++ services/nail/entities.go
@@ -13,10 +13,46 @@ import (
 
 func init() {
 	AddHandler("character", spawnCharacterConsumer)
+	AddHandler("alliance", spawnAllianceConsumer)
 }
 
 func spawnCharacterConsumer(s *Nail, consumer *nsq.Consumer) {
 	consumer.AddHandler(s.wait(nsq.HandlerFunc(s.characterHandler)))
+}
+
+func spawnAllianceConsumer(s *Nail, consumer *nsq.Consumer) {
+	consumer.AddHandler(s.wait(nsq.HandlerFunc(s.allianceHandler)))
+}
+
+func (s *Nail) allianceHandler(message *nsq.Message) error {
+	c := datapackages.Alliance{}
+	err := gobcoder.GobDecoder(message.Body, &c)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	cacheUntil := time.Now().UTC().Add(time.Hour * 24 * 7)
+
+	return s.DoSQL(`
+		INSERT INTO evedata.alliances 
+			(
+				allianceID,
+				name,
+				shortName,
+				executorCorpID,
+				startDate,
+				corporationsCount,
+				updated,
+				cacheUntil
+			)
+			VALUES(?,?,?,?,?,?,UTC_TIMESTAMP(),?) 
+			ON DUPLICATE KEY UPDATE 
+				executorCorpID = VALUES(executorCorpID),
+				corporationsCount = VALUES(corporationsCount), 
+				updated = UTC_TIMESTAMP(), 
+				cacheUntil=VALUES(cacheUntil)
+	`, c.AllianceID, c.Alliance.AllianceName, c.Alliance.Ticker, c.Alliance.ExecutorCorp, c.Alliance.DateFounded.UTC().Format("2006-01-02 15:04:05"), len(c.AllianceCorporations), cacheUntil)
 }
 
 func (s *Nail) characterHandler(message *nsq.Message) error {
