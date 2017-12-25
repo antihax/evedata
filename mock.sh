14ece081de200e93d10607fde9dc7347b9bff07a
antihax <antihax@gmail.com>
2017-12-24 17:22:39 -0800

    allow larger messages

--- mock.sh
+++ mock.sh
@@ -4,5 +4,5 @@ docker run -d -h mock-esi -p 127.0.0.1:8080:8080 antihax/mock-esi
 
 # NSQ Test Service
 docker run --net=host -d -p 4160:4160 -p 4161:4161 -h nsqlookupd1.nsq nsqio/nsq /nsqlookupd
-docker run --net=host -d -p 4171:4171 -h nsqadmin.nsq nsqio/nsq /nsqadmin --lookupd-http-address=127.0.0.1:4161
+docker run --net=host -d -p 4171:4171 -h nsqadmin.nsq nsqio/nsq /nsqadmin --lookupd-http-address=127.0.0.1:4161 -max-msg-size=4194304
 docker run --net=host -d -p 4151:4151 -p 4150:4150 -h localhost nsqio/nsq /nsqd --lookupd-tcp-address=127.0.0.1:4160
