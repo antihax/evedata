4d977f88f20064d6c3bc9c98489c69c066447b2e
antihax <antihax@gmail.com>
2018-01-03 18:33:33 -0800

    Update build

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+go get -u ./...
+
 for dir in cmd/*/; do
     dir=${dir%/}
     if grep -q '^package main$' $dir/*.go 2>/dev/null; then
         echo "building $dir"
         CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo -o bin/$(basename $dir) ./$dir
-        docker build -t antihax/evedata-$(basename $dir) -f Dockerfile.$(basename $dir) .
+        docker build -t antihax/evedata-$(basename $dir) -f docker/Dockerfile.$(basename $dir) .
     else
         echo "(skipped $dir)"
     fi
