4d250740ea020ef4216c7f225a11776bae7d1a04
antihax <antihax@gmail.com>
2018-01-20 19:37:22 -0800

    Simplify the required resources.

--- README.md
+++ README.md
@@ -11,11 +11,7 @@ See @antihax on #devfleet #tweetfleet Slack.
 
 ## Contributing
 
-You will need:
-
-- A MySQL, MariaDB, or Percona server.
-- A redis server.
-- Docker
+You will need Docker for the mock services
 
 ### Services
 
@@ -34,11 +30,7 @@ You will need:
 1. Fork this repository and clone the fork into `gopath/src/github.com/antihax`.
 2. `go get -u ./...` in the repository to install dependencies.
 3. Run ./mock.sh
-4. For testing: run a blank redis on 127.0.0.1:6379 with no authentication ** THIS WILL GET WIPED DURING TESTING **
-5. For testing: run a SQL server on 127.0.0.1:3306 with a blank root password. Import both .sql files from earlier.
-6. Decompress sql.zip and install into a database called `eve`.
-7. Install evedata.sql into a database called `evedata`.
-8. Testing can be configured in a disposable docker env.
+4. Run ./test.sh
 
 Before working on your local copy, please use a seperate branch.
 If there are tests in the package, please make sure you add tests for your work, unless it will hit a public CCP service.
--- mock.sh
+++ mock.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
-# remove all containers
-docker ps -aq | xargs docker stop | xargs docker rm
+
+# Remove any currently running containers
+docker stop mysql teamspeak mock-esi redis nsqlookup nsqadmin nsqd | xargs docker rm
+
+# MySQL Server
+docker run --net=host --name=mysql -d -p 127.0.0.1:3306:3306 -h evedata.sql -e MYSQL_ALLOW_EMPTY_PASSWORD=true percona
+
+# Teamspeak Server
+docker run --net=host --name=teamspeak -d -p 127.0.0.1:9987:9987/udp -p 127.0.0.1:30033:30033 -p 127.0.0.1:10011:10011 -p 127.0.0.1:41144:41144 mbentley/teamspeak clear_database=1 serveradmin_password=nothinguseful
 
 # Mock ESI Server
-docker run -d -h mock-esi -p 127.0.0.1:8080:8080 antihax/mock-esi
+docker run --net=host --name=mock-esi -d  -h mock-esi -p 127.0.0.1:8080:8080 antihax/mock-esi
 
-# NSQ Test Service
-docker run --net=host -d -p 4160:4160 -p 4161:4161 -h nsqlookupd1.nsq nsqio/nsq /nsqlookupd
-docker run --net=host -d -p 4171:4171 -h nsqadmin.nsq nsqio/nsq /nsqadmin --lookupd-http-address=127.0.0.1:4161 -max-msg-size=4194304
-docker run --net=host -d -p 4151:4151 -p 4150:4150 -h localhost nsqio/nsq /nsqd --lookupd-tcp-address=127.0.0.1:4160
-docker run --net=host --name=teamspeak -d -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 mbentley/teamspeak clear_database=1 serveradmin_password=nothinguseful
-sleep 10
+# Redis 
+docker run --net=host --name=redis -d -p 127.0.0.1:6379:6379 -h evedata.sql redis
+
+# NSQ Service
+docker run --net=host --name=nsqlookup -d -p 127.0.0.1:4160:4160 -p 4161:4161 -h nsqlookupd1.nsq nsqio/nsq /nsqlookupd
+docker run --net=host --name=nsqadmin -d -p 127.0.0.1:4171:4171 -h nsqadmin.nsq nsqio/nsq /nsqadmin --lookupd-http-address=127.0.0.1:4161 -max-msg-size=4194304
+docker run --net=host --name=nsqd -d -p 127.0.0.1:4151:4151 -p 4150:4150 -h localhost nsqio/nsq /nsqd --lookupd-tcp-address=127.0.0.1:4160
+
+# Get the admin token for the TS server
 docker exec teamspeak /bin/bash -c 'cat /data/logs/*.log | grep -oE "token=([A-Za-z0-9+_=]+)" | cut -d= -f2' > teamspeakToken.txt
+
+sleep 10
+
+# Populate SQL
+echo "create database eve; create database evedata; set sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO';" | docker exec -i mysql /bin/bash -c mysql
+cat ./services/vanguard/sql/evedata.sql | docker exec -i mysql /bin/bash -c 'mysql -Devedata'
+unzip -p ./services/vanguard/sql/eve.zip | docker exec -i mysql /bin/bash -c 'mysql -Deve'
