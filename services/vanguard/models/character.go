d73a186127b9b6199ffbe8fdc75a99d2316b598e
antihax <antihax@gmail.com>
2018-02-17 12:44:53 -0800

    Rearrange some code

--- services/vanguard/models/character.go
+++ services/vanguard/models/character.go
@@ -253,69 +253,24 @@ func GetCorporationHistory(id int32) ([]CorporationHistory, error) {
 	return ref, nil
 }
 
-type Shares struct {
-	CharacterID        int32  `db:"characterID" json:"characterID,omitempty"`
-	TokenCharacterID   int32  `db:"tokenCharacterID" json:"tokenCharacterID,omitempty"`
-	TokenCharacterName string `db:"tokenCharacterName" json:"tokenCharacterName,omitempty"`
-	EntityID           int32  `db:"entityID" json:"id,omitempty"`
-	EntityName         string `db:"entityName" json:"entityName,omitempty"`
-	Type               string `db:"type" json:"type,omitempty"`
-	Types              string `db:"types" json:"types,omitempty"`
+type Entity struct {
+	EntityID   int32  `db:"entityID" json:"entityID"`
+	EntityName string `db:"entityName" json:"entityName"`
+	EntityType string `db:"entityType" json:"entityType"`
 }
 
+// Obtain Character information by ID.
 // [BENCHMARK] 0.000 sec / 0.000 sec
-func GetShares(characterID int32) ([]Shares, error) {
-	shares := []Shares{}
-	if err := database.Select(&shares, `
-		SELECT S.characterID, S.tokenCharacterID, characterName AS tokenCharacterName, entityID, types, IFNULL(A.name, C.name) AS entityName, IF(A.name IS NULL, "corporation", "alliance") AS type
-		FROM evedata.sharing S
-		INNER JOIN evedata.crestTokens T ON T.tokenCharacterID = S.tokenCharacterID AND T.characterID = S.characterID
-		LEFT OUTER JOIN evedata.corporations C ON C.corporationID = S.entityID
-		LEFT OUTER JOIN evedata.alliances A ON A.allianceID = S.entityID
-		WHERE S.characterID = ?;`, characterID); err != nil {
+func GetEntitiesWithRole(role string) ([]Entity, error) {
+	ref := []Entity{}
+	if err := database.Select(&ref, `
+		SELECT DISTINCT IFNULL(A.allianceID, C.corporationID) AS entityID, IFNULL(A.name, C.name) AS entityName, IF(A.name IS NULL, "corporation", "alliance") AS entityType
+		FROM evedata.crestTokens T
+		LEFT OUTER JOIN evedata.corporations C ON C.corporationID = T.corporationID
+		LEFT OUTER JOIN evedata.alliances A ON A.allianceID = T.allianceID
+		WHERE FIND_IN_SET(?, T.roles) AND T.characterID = ?
+		`, role); err != nil {
 		return nil, err
 	}
-	return shares, nil
-}
-
-func AddShare(characterID, tokenCharacterID, entityID int32, types string) error {
-	if _, err := database.Exec(`
-		INSERT INTO evedata.sharing	(characterID, tokenCharacterID, entityID, types)
-			VALUES(?,?,?,?)
-			ON DUPLICATE KEY UPDATE entityID = entityID, types = VALUES(types)`,
-		characterID, tokenCharacterID, entityID, types); err != nil {
-		return err
-	}
-	return nil
-}
-
-func DeleteShare(characterID, tokenCharacterID, entityID int32) error {
-	if _, err := database.Exec(`DELETE FROM evedata.sharing WHERE characterID = ? AND tokenCharacterID=? AND entityID = ? LIMIT 1`,
-		characterID, tokenCharacterID, entityID); err != nil {
-		return err
-	}
-	return nil
-}
-
-type Service struct {
-	BotServiceID int32  `db:"botServiceID" json:"botServiceID"`
-	Name         string `db:"name" json:"name"`
-	CharacterID  string `db:"characterID" json:"characterID"`
-	EntityID     int32  `db:"entityID" json:"entityID"`
-	Address      string `db:"address" json:"address" `
-	Type         string `db:"type" json:"type"`
-	Services     string `db:"services" json:"services"`
-	Options      string `db:"options" json:"options"`
-}
-
-// [BENCHMARK] 0.000 sec / 0.000 sec
-func GetBotServices(characterID int32) ([]Service, error) {
-	services := []Service{}
-	if err := database.Select(&services, `
-		SELECT botServiceID, name, entityID, address, characterID, type, services, options 
-			FROM evedata.botServices
-			WHERE characterID = ?;`, characterID); err != nil {
-		return nil, err
-	}
-	return services, nil
+	return ref, nil
 }
