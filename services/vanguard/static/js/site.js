2450503cf637d06c560cdfc515ee64fab1143f00
antihax <antihax@gmail.com>
2018-03-03 18:42:20 -0800

    update botdetails

--- services/vanguard/static/js/site.js
+++ services/vanguard/static/js/site.js
@@ -149,6 +149,19 @@
 		});
 	}
 
+	function getUrlVars()
+	{
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++)
+		{
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	}
+
 	function openMarketWindow(id) {
 		if (accountInfo.cursor && accountInfo.cursor.cursorCharacterID > 0) {
 			$.ajax({
