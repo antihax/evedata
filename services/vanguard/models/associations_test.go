eb411fbdd8b40e94766c6e9ceb825c89dc3ae046
antihax <antihax@gmail.com>
2018-03-29 19:13:31 -0700

    move associations to artifice

--- services/vanguard/models/associations_test.go
+++ services/vanguard/models/associations_test.go
@@ -1,9 +1,7 @@
 package models
 
 import (
-	"log"
 	"testing"
-	"time"
 )
 
 func TestGetCharacterKnownAssociates(t *testing.T) {
@@ -36,47 +34,5 @@ func TestGetAllianceKnownAssociates(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 		return
-	}
-}
-
-func TestBuildRelationships(t *testing.T) {
-
-	err := UpdateCharacter(1001, "dude", 1, 1, 147035273, 0, 1, "male", -10, time.Now())
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	err = UpdateCharacter(1002, "dude 2", 1, 1, 147035273, 0, 2, "Female", -10, time.Now())
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	err = UpdateCharacter(1003, "dude 3", 1, 1, 147035273, 0, 2, "Female", -10, time.Now())
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	err = UpdateCorporationHistory(1001, 147035273, 100000222, time.Now())
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	err = UpdateCorporationHistory(1002, 147035273, 100000223, time.Now())
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	err = UpdateCorporationHistory(1003, 147035273, 100000224, time.Now())
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	err = BuildRelationships()
-	if err != nil {
-		t.Error(err)
 	}
 }
