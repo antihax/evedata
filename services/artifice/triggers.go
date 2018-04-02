8d26341ab9dcbcca6b848f1cb22ebaf328c691d0
antihax <antihax@gmail.com>
2018-04-02 16:33:24 -0700

    prevent running dupe triggers

--- services/artifice/triggers.go
+++ services/artifice/triggers.go
@@ -13,14 +13,15 @@ var (
 type triggerFunc func(*Artifice) error
 
 type trigger struct {
-	name   string
-	f      triggerFunc
-	ticker *time.Ticker
+	name    string
+	f       triggerFunc
+	ticker  *time.Ticker
+	running bool
 }
 
 // Register a trigger to a queue operation.
 func registerTrigger(name string, f triggerFunc, ticker *time.Ticker) {
-	triggers = append(triggers, trigger{name, f, ticker})
+	triggers = append(triggers, trigger{name, f, ticker, false})
 }
 
 func (s *Artifice) runTriggers() {
@@ -31,14 +32,25 @@ func (s *Artifice) runTriggers() {
 		}
 		chosen, _, ok := reflect.Select(cases)
 		if ok {
-			t := triggers[chosen]
+			go func(t *trigger, s *Artifice) {
+				// prevent running duplicate tasks
+				if !t.running {
+					// set running state
+					t.running = true
 
-			go func(t trigger, s *Artifice) {
-				err := t.f(s)
-				if err != nil {
-					log.Println(err)
+					// unset running state when the function ends
+					defer func(b *bool) {
+						*b = false
+					}(&t.running)
+
+					// run the task
+					if err := t.f(s); err != nil {
+						log.Println(err)
+					}
+				} else {
+					log.Printf("already running %s\n", t.name)
 				}
-			}(t, s)
+			}(&triggers[chosen], s)
 		}
 	}
 }
