a583f35d9af75078fed5968cf99851ff41a8ec1f
antihax <antihax@gmail.com>
2018-04-18 16:37:18 -0700

    Add mail user

--- services/tokenserver/api.go
+++ services/tokenserver/api.go
@@ -2,6 +2,8 @@ package tokenserver
 
 import (
 	"context"
+	"errors"
+	"strings"
 
 	"golang.org/x/oauth2"
 	"google.golang.org/grpc"
@@ -39,5 +41,40 @@ func GetTokenHandler(srv interface{}, ctx context.Context, dec func(interface{})
 
 // GetToken gets a token from the store and returns it
 func (s TokenServer) GetToken(ctx context.Context, t *TokenRequest) (*oauth2.Token, error) {
+	return s.tokenStore.GetToken(t.CharacterID, t.TokenCharacterID)
+}
+
+type MailUserRequest struct {
+	Username string
+	Password string
+}
+
+func GetMailUserHandler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MailUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	return srv.(*TokenServer).GetMailUser(ctx, in)
+}
+
+// GetToken gets a token from the store and returns it
+func (s TokenServer) GetMailUser(ctx context.Context, u *MailUserRequest) (*oauth2.Token, error) {
+	type mailUser struct {
+		CharacterID      int32 `db:"characterID"`
+		TokenCharacterID int32 `db:"tokenCharacterID"`
+	}
+	t := mailUser{}
+
+	if err := s.db.QueryRowx(
+		`	SELECT characterID, tokenCharacterID FROM evedata.crestTokens
+			WHERE characterName = ? AND mailPassword = ? AND mailPassword != ""
+			LIMIT 1;`).StructScan(&t); err != nil {
+		// Ignore this error.
+		if strings.Contains(err.Error(), "no rows in result set") {
+			return nil, errors.New("Access Denied")
+		}
+		return nil, err
+	}
+
 	return s.tokenStore.GetToken(t.CharacterID, t.TokenCharacterID)
 }
--- services/tokenserver/tokenServer_test.go
+++ services/tokenserver/tokenServer_test.go
@@ -1,6 +1,19 @@
 package tokenserver
 
-/*
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/antihax/evedata/internal/msgpackcodec"
+	"github.com/antihax/evedata/internal/redigohelper"
+	"github.com/antihax/evedata/internal/sqlhelper"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc"
+)
+
 var tokenserver *TokenServer
 
 func TestMain(m *testing.M) {
@@ -42,4 +55,3 @@ func TestTokens(t *testing.T) {
 	err = r.Invoke(context.Background(), "/TokenStore/GetToken", &TokenRequest{CharacterID: 555, TokenCharacterID: 555}, &token)
 	assert.Nil(t, err)
 }
-*/
