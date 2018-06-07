9600de371be5933402b8107d616a74680638ea99
antihax <antihax@gmail.com>
2018-06-07 16:26:06 -0700

    refactor k8s setup

--- kubernetes/infrastructure/setup.sh
+++ kubernetes/infrastructure/setup.sh
@@ -2,3 +2,6 @@ curl -fsSL https://raw.githubusercontent.com/appscode/voyager/7.0.0/hack/deploy/
     | bash -s -- --provider=baremetal --rbac
 
 kubectl apply -f evedata.yaml
+kubectl apply -f monitoring.yaml
+kubectl apply -f nsqd.yaml
+kubectl apply -f ledis-redis.yaml
