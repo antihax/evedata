22feef2c6aeb42f4e3f4923754a6ce55148aa525
antihax <antihax@gmail.com>
2018-06-23 12:52:22 -0700

    typo

--- kubernetes/services/setup.sh
+++ kubernetes/services/setup.sh
@@ -6,6 +6,6 @@ kubectl apply -f conservator.yaml
 kubectl apply -f hammer.yaml
 kubectl apply -f nail.yaml
 kubectl apply -f mailserver.yaml
-kubectl apply -f zkilboard.yaml
+kubectl apply -f zkillboard.yaml
 kubectl apply -f vanguard.yaml
 kubectl apply -f artifice.yaml
