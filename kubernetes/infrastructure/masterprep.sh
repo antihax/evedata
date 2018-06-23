86f9144b5da3a052633c3b6aad5d10a05e7324d9
antihax <antihax@gmail.com>
2018-06-23 07:36:37 -0700

    update setup

---
+++ kubernetes/infrastructure/masterprep.sh
@@ -0,0 +1,33 @@
+sed -i 's/^#*Port 22/Port 44/' /etc/ssh/sshd_config
+service sshd restart
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list  
+deb http://apt.kubernetes.io/ kubernetes-xenial main  
+EOF
+
+mkdir -p /etc/cni/net.d
+
+apt update -y
+apt install -y docker.io apt-transport-https pwgen
+apt update -y
+apt install -y kubelet kubeadm kubectl kubernetes-cni
+apt upgrade -y
+apt autoremove -y
+
+systemctl enable docker.service
+
+kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export kubeconfig=/etc/kubernetes/admin.conf
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl create secret -n kube-system generic weave-passwd --from-literal=weave-passwd=`pwgen 32 1 -sy`
+kubectl patch daemonset -n kube-system --type=json weave-net -p '[{"op": "add", "path": "/spec/template/spec/containers/0/env/1", "value": {"name":"WEAVE_PASSWORD","valueFrom":{"secretKeyRef":{"name":"weave-passwd","key":"weave-passwd"}}}}]'
+kubectl delete pod --all -n kube-system
---
+++ kubernetes/infrastructure/workerprep.sh
@@ -0,0 +1,23 @@
+sed -i 's/^#*Port 22/Port 44/' /etc/ssh/sshd_config
+service sshd restart
+
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list  
+deb http://apt.kubernetes.io/ kubernetes-xenial main  
+EOF
+
+mkdir -p /etc/cni/net.d
+
+apt update -y
+apt install -y docker.io apt-transport-https
+apt update -y
+apt install -y kubelet kubeadm kubectl kubernetes-cni
+apt upgrade -y
+apt autoremove -y
+
+systemctl enable docker.service
+
+#kubectl label nodes platinum loadbalancer=voyager
