21dfa468dfde22d24c551f4afbcfdcc0678d5765
antihax <antihax@gmail.com>
2018-07-24 15:25:53 -0700

    make this granular

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
-go get -u ./...
-
-for dir in cmd/*/; do
-    dir=${dir%/}
-    if grep -q '^package main$' $dir/*.go 2>/dev/null; then
+if [[ -z "$1" ]] 
+then
+    for dir in cmd/*/; do
+        dir=${dir%/}
+        if grep -q '^package main$' $dir/*.go 2>/dev/null; then
+            echo "building $dir"
+            CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo -o bin/$(basename $dir) ./$dir
+            docker build -t antihax/evedata-$(basename $dir) -f docker/Dockerfile.$(basename $dir) .
+        else
+            echo "(skipped $dir)"
+        fi
+    done
+else
+    dir=cmd/$1/
         echo "building $dir"
         CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo -o bin/$(basename $dir) ./$dir
         docker build -t antihax/evedata-$(basename $dir) -f docker/Dockerfile.$(basename $dir) .
-    else
-        echo "(skipped $dir)"
-    fi
-done
+        docker push antihax/evedata-$(basename $dir)
+fi
--- dockerpush.sh
+++ dockerpush.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
-for dir in cmd/*/; do
-    dir=${dir%/}
-    if grep -q '^package main$' $dir/*.go 2>/dev/null; then
-        docker push antihax/evedata-$(basename $dir)
-    else
-        echo "(skipped $dir)"
-    fi
-done
+if [[ -z "$1" ]] 
+then
+    for dir in cmd/*/; do
+        dir=${dir%/}
+        if grep -q '^package main$' $dir/*.go 2>/dev/null; then
+            docker push antihax/evedata-$(basename $dir)
+        else
+            echo "(skipped $dir)"
+        fi
+    done
+else
+    dir=$1
+    docker push antihax/evedata-$dir
+fi
