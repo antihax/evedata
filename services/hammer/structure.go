8ae7963e954df91a1543bf7e2b6ab00837056237
antihax <antihax@gmail.com>
2018-08-04 15:00:07 -0700

    remove market order processing

--- services/hammer/structure.go
+++ services/hammer/structure.go
@@ -7,13 +7,10 @@ import (
 
 	"github.com/antihax/evedata/internal/datapackages"
 	"github.com/antihax/goesi"
-	"github.com/antihax/goesi/esi"
-	"github.com/antihax/goesi/optional"
 )
 
 func init() {
 	registerConsumer("structure", structureConsumer)
-	registerConsumer("structureOrders", structureOrdersConsumer)
 }
 
 func structureConsumer(s *Hammer, parameter interface{}) {
@@ -35,48 +32,6 @@ func structureConsumer(s *Hammer, parameter interface{}) {
 	}
 	// Send out the result
 	err = s.QueueResult(&datapackages.Structure{Structure: struc, StructureID: structureID}, "structure")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-}
-
-func structureOrdersConsumer(s *Hammer, parameter interface{}) {
-	structureID := parameter.(int64)
-	var page int32 = 1
-	orders := []esi.GetMarketsStructuresStructureId200Ok{}
-
-	if s.inQueue.CheckWorkExpired("evedata_structure_market_failure", structureID) {
-		return
-	}
-
-	ctx := context.WithValue(context.Background(), goesi.ContextOAuth2, *s.token)
-	for {
-		o, _, err := s.esi.ESI.MarketApi.GetMarketsStructuresStructureId(ctx, structureID,
-			&esi.GetMarketsStructuresStructureIdOpts{
-				Page: optional.NewInt32(page),
-			})
-		if err != nil {
-			if strings.Contains(err.Error(), "403") {
-				s.inQueue.SetWorkExpire("evedata_structure_market_failure", structureID, 86400)
-			}
-			log.Println(err)
-			return
-		} else if len(o) == 0 { // end of the pages
-			break
-		}
-
-		orders = append(orders, o...)
-
-		page++
-	}
-	// early out if there are no orders
-	if len(orders) == 0 {
-		return
-	}
-
-	// Send out the result
-	err := s.QueueResult(&datapackages.StructureOrders{Orders: orders, StructureID: structureID}, "structureOrders")
 	if err != nil {
 		log.Println(err)
 		return
