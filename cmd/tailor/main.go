4f2f3d5cdb158930cbfa088c78e3e2722ab27f67
antihax <antihax@gmail.com>
2018-09-03 15:32:04 -0700

    revert

--- cmd/tailor/main.go
+++ cmd/tailor/main.go
@@ -11,8 +11,9 @@ import (
 	"github.com/antihax/evedata/internal/nsqhelper"
 	"github.com/antihax/evedata/internal/sqlhelper"
 	"github.com/antihax/evedata/services/tailor"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	backblaze "gopkg.in/kothar/go-backblaze.v0"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -21,6 +22,28 @@ func main() {
 
 	db := sqlhelper.NewDatabase()
 
+	/*
+		b2, err := backblaze.NewB2WithHTTPClient(backblaze.Credentials{
+			AccountID:      os.Getenv("B2_ACCOUNTID"),
+			ApplicationKey: os.Getenv("B2_APPLICATION_KEY"),
+		}, &http.Client{
+			Transport: &tailor.ApiTransport{
+				Next: &http.Transport{
+					MaxIdleConns: 200,
+					DialContext: (&net.Dialer{
+						Timeout:   300 * time.Second,
+						KeepAlive: 5 * 60 * time.Second,
+						DualStack: true,
+					}).DialContext,
+					IdleConnTimeout:       5 * 60 * time.Second,
+					TLSHandshakeTimeout:   10 * time.Second,
+					ResponseHeaderTimeout: 60 * time.Second,
+					ExpectContinueTimeout: 0,
+					MaxIdleConnsPerHost:   20,
+				},
+			}})
+			b2.Debug = true
+	*/
 	b2, err := backblaze.NewB2(backblaze.Credentials{
 		AccountID:      os.Getenv("B2_ACCOUNTID"),
 		ApplicationKey: os.Getenv("B2_APPLICATION_KEY"),
