1bbc53fb9e05432eef7047bceb62da2e54e67fc2
antihax <antihax@gmail.com>
2019-11-06 15:07:39 -0800

    They went uppercase first...

--- services/vanguard/views/common.go
+++ services/vanguard/views/common.go
@@ -23,23 +23,23 @@ type OpenGraph struct {
 func entityImage(entityID int64, entityType string, size int) string {
 	switch entityType {
 	case "character":
-		return "https://imageserver.eveonline.com/character/" +
+		return "https://imageserver.eveonline.com/Character/" +
 			strconv.FormatInt(entityID, 10) +
 			"_" + strconv.Itoa(size) + ".jpg"
 	case "corporation":
-		return "https://imageserver.eveonline.com/corporation/" +
+		return "https://imageserver.eveonline.com/Corporation/" +
 			strconv.FormatInt(entityID, 10) +
 			"_" + strconv.Itoa(size) + ".png"
 	case "alliance":
-		return "https://imageserver.eveonline.com/alliance/" +
+		return "https://imageserver.eveonline.com/Alliance/" +
 			strconv.FormatInt(entityID, 10) +
 			"_" + strconv.Itoa(size) + ".png"
 	case "type":
-		return "https://imageserver.eveonline.com/type/" +
+		return "https://imageserver.eveonline.com/Type/" +
 			strconv.FormatInt(entityID, 10) +
 			"_" + strconv.Itoa(size) + ".png"
 	case "render":
-		return "https://imageserver.eveonline.com/render/" +
+		return "https://imageserver.eveonline.com/Render/" +
 			strconv.FormatInt(entityID, 10) +
 			"_" + strconv.Itoa(size) + ".png"
 	}
